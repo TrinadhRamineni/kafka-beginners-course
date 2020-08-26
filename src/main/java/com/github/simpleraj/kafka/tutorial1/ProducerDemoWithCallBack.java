75ac73b1df44b35b4f5ea54734fe21d64edd1d19
Prudhvi raj <prudhviraj5758@gmail.com>
2020-08-26 13:02:41 -0500

    Commiting Rollback logic in producer class with a loop of records to kafka

---
+++ src/main/java/com/github/simpleraj/kafka/tutorial1/ProducerDemoWithCallBack.java
@@ -0,0 +1,53 @@
+package com.github.simpleraj.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallBack {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallBack.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+        // Create producer configurations
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the kafka producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 14; i++) {
+            // create a producer record
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>("first_topic", "Hello world" + Integer.toString(i));
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //execute everytime record is successfully sent ot error is thrown
+                    if (e == null) {
+                        // record is successfully sent
+                        logger.info("Received new Metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        // record is not successfully sent
+                        logger.error("Error while producing data to kafka: ", e);
+                    }
+                }
+            });
+        }
+        // flush data to consumer
+        producer.flush();
+        // flush and close application
+        producer.close();
+    }
+}
