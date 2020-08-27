76c74220e51f2d4b72ba47494fa588ce0cfa6975
Prudhvi raj <prudhviraj5758@gmail.com>
2020-08-27 18:29:13 -0500

    Safe Producer Configurations added-->Idempotent Producer

--- src/main/java/com/github/simpleraj/kafka/tutorial1/ProducerDemoWithCallBack.java
+++ src/main/java/com/github/simpleraj/kafka/tutorial1/ProducerDemoWithCallBack.java
@@ -20,6 +20,12 @@ public class ProducerDemoWithCallBack {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // Create a safe idempotent producer which helps in removing duplicates and maintain ordering.
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
         // Create the kafka producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
